37216dc3ea0030dd90fa01045e23580eeeec433f
tobiasvavroch <tbv@email.cz>
2025-03-03 14:08:16 +0100

    train StyleGAN2 with encoer improve

--- src/StyleGAN2/train_with_encoder.py
+++ src/StyleGAN2/train_with_encoder.py
@@ -11,15 +11,15 @@ set_seed()
 
 
 if __name__ == '__main__':
-    model = StyleGan2Trainer(name='model_with_encoder', 
-                             models_dir='./my_data/StyleGAN2/', 
-                             pre_generated_uv_textures_dir='my_data/uv_textures_128',
+    model = StyleGan2Trainer(name='model_with_encoder_backup', 
+                             models_dir='./fresh_data/StyleGAN2', 
+                             pre_generated_uv_textures_dir='/home/tobiasvavroch/generating-textures-with-a-gan/my_data/uv_textures_128',
                              uv_textures_pregenerated=True,
                              conditional_input=True,
                              image_size=256,
                              texture_size=128,
-                             evaluate_every=10
                              )
-
+    
     model.prepare_for_training()
     model.train_model()
+
